f5580dea717a573944d67392603be960d7769751
Eduardo Nola <edunola13@hotmail.com>
2018-02-15 10:17:26 -0300

    Comandos del Framework

---
+++ src/Commands/Project.php
@@ -0,0 +1,11 @@
+<?php
+namespace Enola\Commands;
+use Enola\Cron;
+use Enola\Cron\En_CronRequest, Enola\Support\Response;
+
+class Project extends Cron\En_CronController{
+    
+    public function create(En_CronRequest $request, Response $response){        
+        
+    }
+}
